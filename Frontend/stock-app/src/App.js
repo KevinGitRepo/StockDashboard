df1fb6f8f3128f6206e35b4bf8a5471796bacd8f
Kevin <ksakowicz10@gmail.com>
2025-03-11 17:22:45 -0400

    Added graph from alpha vantage api data

--- Frontend/stock-app/src/App.js
+++ Frontend/stock-app/src/App.js
@@ -1,35 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
-import axios from 'axios'
+import {LineChart} from "./LineChart";
 
 function App() {
-    const [ data, setData ] = useState('');
-    const [ symbol, setSymbol ] = useState('');
-
-    const getData = async () => {
-        console.log(symbol);
-        const url = 'http://localhost:4999/get';
-        const params = {
-            symbol: symbol,
-        }
-        try {
-            const response = await axios.get(url, {params});
-            setData(JSON.stringify(response.data));
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    const onSymbolChange = (symbol) => {
-        setSymbol(symbol.target.value);
-    }
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-        <input type="text" value={symbol} onChange={onSymbolChange}/>
-        <button onClick={getData}>Get Data</button>
-        <p>{data}</p>
+      <LineChart />
     </div>
   );
 }
